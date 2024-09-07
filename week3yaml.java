3bc3b8d32c603ef1c29768fedd12994ca6c903cd
kalyankr4914323 <147293688+kalyankr4914323@users.noreply.github.com>
2024-09-07 10:08:34 +0530

    Create week3yaml.java

---
+++ week3yaml.java
@@ -0,0 +1,211 @@
+name: CI
+on: push
+jobs:
+  build:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v3
+      - name: Run Jenkinsfile
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11'
+      - name: Execute pipeline
+        uses: actions/setup-java@v3
+        with:
+          java-version: '11
